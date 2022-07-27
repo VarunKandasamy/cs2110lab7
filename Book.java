70d52eea9cf1f8bedbfd92ed7689bc212f58893f
VarunKandasamy <38441113+VarunKandasamy@users.noreply.github.com>
2022-07-27 14:50:54 -0400

    Added author to Book and updated README

--- Book.java
+++ Book.java
@@ -1,10 +1,12 @@
 /** A class modeling a book. */
 public class Book {
     String title;
+    String author;
 
     /** Create a new book with title b. */
-    public Book(String t) {
+    public Book(String t, String a) {
         title= t;
+        author = a;
     }
 
     /** Print this book. */
@@ -14,7 +16,7 @@ public class Book {
 
     /** Some book examples. */
     public static void main(String[] args) {
-        Book b = new Book("A Clockwork Orange");
+        Book b = new Book("A Clockwork Orange", "Anthony Burgess");
         b.printBook();
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 An example git repository for CS 2110.
+The Book object contains a String title and a String author.
